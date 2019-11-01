48eaa97a1ea878bcc087e064c8b07360deddc8ec
Alec Paulson <alec.paulson@vantiv.com>
2019-11-01 11:29:46 -0700

    Initial commit to redesign

---
+++ server/app.js
@@ -0,0 +1,26 @@
+var StaticServer = require('static-server');
+var server = new StaticServer({
+  rootPath: '.',            // required, the root of the server file tree
+  port: 8080,               // required, the port to listen
+  //name: 'my-http-server',   // optional, will set "X-Powered-by" HTTP header
+  //host: '10.0.0.100',       // optional, defaults to any interface
+  //cors: '*',                // optional, defaults to undefined
+  //followSymlink: true,      // optional, defaults to a 404 error
+  templates: {
+    index: 'foo.html',      // optional, defaults to 'index.html'
+    notFound: '404.html'    // optional, defaults to undefined
+  }
+});
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/hello', (req, res) => 
+res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+ 
+server.start(function () {
+  console.log('Server listening to', server.port);
+});
